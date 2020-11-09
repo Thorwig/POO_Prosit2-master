bb9f53d2ab7c59e0a83431d7ae6899b176d25636
Mouad NOSSAIR <mouad.nossair@viacesi.fr>
2020-11-09 08:44:46 +0100

    Ajoutez des fichiers projet.

---
+++ Prosit_2/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "fichier.h"
+#include "cesar.h"
+
+using namespace std;
+
+void binaire(int chiffre) {
+
+}
+
+int main(){
+	
+	Fichier fichier;
+	Cesar test;
+	string dechiffre, chiffre;
+	dechiffre = fichier.lire("Texte.txt");
+	chiffre = test.decrypter(dechiffre);
+	test.crypter(chiffre);
+	return 0;
+}
